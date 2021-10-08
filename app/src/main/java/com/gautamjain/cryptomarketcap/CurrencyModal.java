cd85d2b809a5f49e27d01206de4e7d227c61db58
gautamjain09 <gautamjain0918@gmail.com>
2021-10-08 06:40:24 +0530

    Main Activity UI

---
+++ app/src/main/java/com/gautamjain/cryptomarketcap/CurrencyModal.java
@@ -0,0 +1,31 @@
+package com.gautamjain.cryptomarketcap;
+
+public class CurrencyModal {
+    private String name;
+    private String symbol;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
--- app/src/main/java/com/gautamjain/cryptomarketcap/MainActivity.java
+++ app/src/main/java/com/gautamjain/cryptomarketcap/MainActivity.java
@@ -1,14 +1,29 @@
 package com.gautamjain.cryptomarketcap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private ProgressBar progressBar;
+    private RecyclerView recyclerView;
+    private List<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.EditTextSearch);
+        progressBar = findViewById(R.id.ProgressBar);
+        recyclerView = findViewById(R.id.RecyclerView);
+
     }
 }
